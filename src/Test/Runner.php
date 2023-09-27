122e89fe0484d508446662cdd99b96fa84430a69
Pablo <pablo@bolboreta.org>
2023-09-27 22:29:16 +0100

    Testing tool added.

---
+++ src/Test/Runner.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\Test;
+
+class Runner
+{
+	public function __construct(
+		private string $path,
+	) {
+	}
+
+	public function run(): void
+	{
+	}
+}
